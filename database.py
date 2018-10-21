37079c20253f70729e02f09e07dcec8d7c0f92f5
Abhishek S P <abhisheksp1993@gmail.com>
2018-10-20 17:14:43 -0700

    [SP] Flask - Persistence layer

---
+++ database.py
@@ -0,0 +1,16 @@
+from pymongo import MongoClient
+import json
+
+with open('mongo.json') as f:
+    data = json.load(f)
+    uri = data['uri']
+    port = int(data['port'])
+    client = MongoClient(uri, port)
+    db = client.data
+    transactions_collection = db.raw2
+
+
+def save_transactions(transaction_entries):
+    new_result = transactions_collection.insert_many(transaction_entries)
+    print('Inserted {} entries'.format(len(new_result.inserted_ids)))
+    print('New IDs: {}'.format(new_result.inserted_ids))
--- transactions.py
+++ transactions.py
@@ -1,12 +1,13 @@
 import datetime
 from functools import partial
 from operator import is_not
-
+import copy
 import requests
 
 import account
 import cobrand
 import user
+from database import save_transactions
 
 
 def transform(username, transaction_entries, accounts_dict):
@@ -25,15 +26,16 @@ def transform(username, transaction_entries, accounts_dict):
             "bank": account_name,
             "balance": balance,
             "amount": amount,
-            "timeStamp": timestamp,
+            "timeStamp": timestamp.strftime('%Y-%m-%d %H-%M-%S'),
             "date": timestamp.day,
             "month": timestamp.month,
             "year": timestamp.year
         }
 
     timestamp_transform = lambda t: datetime.datetime.strptime(t, "%Y-%m-%dT%H:%M:%SZ")
-    transformed_transactions = filter(partial(is_not, None), map(transform_transaction, transaction_entries))
-    return list(transformed_transactions)
+    transformed_transactions = list(filter(partial(is_not, None), map(transform_transaction, transaction_entries)))
+    save_transactions(copy.deepcopy(transformed_transactions))
+    return transformed_transactions
 
 
 def transactions(username, password, from_date):
